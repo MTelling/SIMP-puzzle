b414f73f7c720c73b980bd7a36fdcd2ca3096129
SuperPagh <mpagh5@hotmail.com>
2016-01-04 12:42:25 +0100

    Version 1.0 of Board class
    Makes a 2D array of tiles and leaves last spot unused

---
+++ src/Board.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Board {
+
+	protected Tile[][] grid;
+	
+	public Board(int n)
+	{
+		
+		grid = new Tile[n][n];
+		
+		int[] uniqueNumbers = getRandomSequence(n);
+		
+		int counter = 0;
+		for(int y = 0; y < n; y++)
+		{
+			
+			for(int x = 0; x < n; x++)
+			{
+				// We need only n^2 - 1 tiles. Last corner needs to be empty.
+				if( !(x == n-1 && y == n-1) )
+				{
+					grid[x][y] = new Tile(uniqueNumbers[counter]);
+					counter++;
+				}
+				// Don't put a tile on the last space.
+			}
+		}
+		
+		
+		
+	}
+	
+	private int[] getRandomSequence(int n)
+	{
+		if(n < 1)
+		{
+			throw new IllegalArgumentException("Random Sequence Length cannot be less than one!");
+		}
+		
+		return new Random().ints(1, n).distinct().limit(n).toArray();
+	}
+}
