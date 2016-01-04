6d953207777015b05f6ed085bb4bf0606493bb9d
SuperPagh <mpagh5@hotmail.com>
2016-01-04 13:21:15 +0100

    Tiles is now movable

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,10 @@
+import java.awt.Point;
 import java.util.Random;
 
 public class Board {
 
 	protected Tile[][] grid;
+	protected Point emptySpot;
 	
 	public Board(int n)
 	{
@@ -23,10 +25,42 @@ public class Board {
 					grid[x][y] = new Tile(uniqueNumbers[counter]);
 					counter++;
 				}
-				// Don't put a tile on the last space.
+				else
+				{
+					// Don't put a tile on the last space.
+					// save coordinates for the empty spot though
+					emptySpot = new Point (x,y);
+				}
 			}
 		}
 		
+	}
+	
+	public boolean moveTile(int x, int y)
+	{
+		// Check if the empty spot is a neighbor
+		if ( (x == emptySpot.x && ( y == emptySpot.y - 1 || y == emptySpot.y + 1 ))
+				|| (y == emptySpot.y && ( x == emptySpot.x - 1 || x == emptySpot.x + 1 )) )
+		{
+			// If so, switch tile with empty spot
+			
+			grid[emptySpot.x][emptySpot.y] = grid[x][y];
+			
+			emptySpot.setLocation(x, y);			
+			
+			// Return true
+			return true;
+		}
+		else
+		{
+			// Empty spot was not a neighbor. Return false
+			return false;
+		}
+	}
+	
+	public Point getEmptySpot()
+	{
+		return emptySpot;
 	}
 	
 	private int[] getRandomSequence(int n)
