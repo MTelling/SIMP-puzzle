0ea62645b4d3fbcc9aaa1ce8a484e5c79ccafac5
SuperPagh <mpagh5@hotmail.com>
2016-01-04 11:03:09 +0100

    JUST DO IT!
    Do it.

--- src/SimpDriver.java
+++ src/SimpDriver.java
@@ -1,6 +1,15 @@
 
 public class SimpDriver {
 	public static void main(String[] args) {
-		System.out.println("Du er en tissemand! HIHIHIHIHIHI");
+		System.out.println("Du er en tissemand! HIHIHIHIHIHI\n\n\n");
+		
+		System.out.println("Shia is how strong?");
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.println(i);
+		}
+		System.out.println("SHIA IS 10 STRONG!");
+		System.out.println("STRONG LIKE A BULL!");
+		
 	}
 }
