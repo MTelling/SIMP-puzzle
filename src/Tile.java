44b9178e0faba3bd4fbea851825b8f713a6fa170
SuperPagh <mpagh5@hotmail.com>
2016-01-04 12:32:50 +0100

    Removed coordinates from Tile class

--- src/Tile.java
+++ src/Tile.java
@@ -1,26 +1,15 @@
-import java.awt.Point;
 
 public class Tile 
 {
 
-	protected Point where;
 	protected int tileNum;
 	
 	
 	public Tile (int x, int y, int num)
 	{
-		where = new Point(x,y);
 		tileNum = num;
 	}
 	
-	public int getx()
-	{
-		return where.x;
-	}
-	
-	public int gety()
-	{
-		return where.y;
-	}
+
 	
 }
