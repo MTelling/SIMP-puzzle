8fe583655804f6cf19d11f2774019b72c0650c78
SuperPagh <mpagh5@hotmail.com>
2016-01-07 18:53:23 +0100

    Highscore object ready for testing
    I've put everything in one object, instead of having to create several
    Highscore instances. This is to save the scores to one file only.

--- src/Highscore.java
+++ src/Highscore.java
@@ -6,29 +6,121 @@ public class Highscore implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private static final int HIGHSCORE_SIZE = 5;
-	private int boardSize;
-	private String[] highScoreNames;
-	private int[] highScores;
 	
-	public Highscore(int boardSize) {
-		this.boardSize = boardSize;
+	private static final String FILE_NAME = "resources/Highscores";
+	
+	private static final String STD_NAME = "Robert";
 		
-		this.highScoreNames = new String[HIGHSCORE_SIZE];
-		this.highScores = new int[HIGHSCORE_SIZE];
+	private int currentBoardSize;
+	private String[] highScoreNames;
+	private int[] scores;
+	
+	private Map<Integer, HashMap<String, Integer>> highScores = new HashMap<Integer, HashMap<String, Integer>>();
+	
+	
+	public Highscore(int currentBoardSize) {
+		
+		Object obj = SaveLoad.loadFromFile(FILE_NAME);
+		
+	    scores = new int[HIGHSCORE_SIZE];
+	    highScoreNames = new String[HIGHSCORE_SIZE];
+	    
+	    
+		if(obj instanceof Highscore) {
+			// Get the previous Highscores if it exists
+			Highscore temp = (Highscore) obj;
+			this.highScores = temp.getHighscoreMap();
+			
+			if (this.highScores.get(currentBoardSize) != null) {
+			    HashMap<String, Integer> tempScores = this.highScores.get(currentBoardSize);
+			    
+			    
+			    int tempCounter = 0; //TODO: I know this is fugly. Can anyone make it prettier?
+			    for (Map.Entry<String, Integer> entry : tempScores.entrySet()) {
+			    	scores[tempCounter] = entry.getValue();
+			    	highScoreNames[tempCounter] = entry.getKey();
+			    	tempCounter++;
+			    	
+			    	 // Just a security measure in case you load in an old Highscorefile, with too many entries
+			    	if(tempCounter == HIGHSCORE_SIZE) { break; }
+			    }
+			    
+			    // Add possible missing scores.
+			    if(tempCounter < HIGHSCORE_SIZE) {
+			    	addMissingScores(tempCounter);
+			    }
+			    
+			} else {
+			    // No such key; highscores for this board is nonexistent.
+				
+			    addMissingScores(0);
+				
+			}
+			
+		} else {
+			// No previous Highscores found; Create new list.
+			
+			addMissingScores(0);
+			
+			for(int i = 0; i < HIGHSCORE_SIZE; i++) {
+				HashMap<String, Integer> temp = new HashMap<String, Integer>();
+				temp.put(highScoreNames[i], scores[i]);
+				highScores.put(currentBoardSize , temp);
+			}
+			
+		}
+		
+		this.currentBoardSize = currentBoardSize;
+	}
+	
+	public void addMissingScores(int fromWhere) {
+		for(int i = fromWhere; i < HIGHSCORE_SIZE; i++) {
+			this.scores[i] = 0;
+			this.highScoreNames[i] = STD_NAME;
+		}
+	}
+	
+	public HashMap<String, Integer> getHighscores() {
+		return highScores.get(currentBoardSize);
+	}
+	
+	public Map<Integer, HashMap<String, Integer>> getHighscoreMap() {
+		return highScores;
 	}
 	
 	public boolean isHighscore(int score) {
 		
+		
+		for(int i = 0; i < HIGHSCORE_SIZE; i++) {
+			if (score > scores[i]) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	
-	public Map<String, Integer> getHighscores() {
-		Map<String, Integer> temp = new HashMap<String, Integer>();
-		for(int i = 0; i < HIGHSCORE_SIZE; i++) {
-			temp.put(highScoreNames[i], highScores[i]);
+	public void addHighScore (String name, int score) {
+		int scoreSpot = HIGHSCORE_SIZE - 1;
+		for(int i = scoreSpot; i >= 0 ; i++) {
+			if(score > scores[i]) {
+				scoreSpot = i;
+			}
 		}
 		
-		return temp;
+		scores[scoreSpot] = score;
+		this.highScoreNames[scoreSpot] = name;
+		
 	}
+	
+	public void updateHighScores () {
+		HashMap<String, Integer> temp = new HashMap<String, Integer>();
+		for(int i = 0; i < HIGHSCORE_SIZE; i++) {
+			temp.put(highScoreNames[i], scores[i]);
+		}
+		highScores.put(currentBoardSize , temp);
+	}
+	
+
 	
 }
