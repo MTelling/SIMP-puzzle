a25b022ce3b7888ec3efa920660acfd60a30447e
SuperPagh <mpagh5@hotmail.com>
2016-01-08 14:50:17 +0100

    Added Tile object
    int number;
    Point coords;
    void translate();
    int getNumber();
    Point getCoords();

---
+++ src/Tile.java
@@ -0,0 +1,26 @@
+import java.awt.Point;
+
+public class Tile {
+
+	private int number;
+	private Point coords;
+	
+	public Tile (int tileNum, Point tilePos) {
+		this.number = tileNum;
+		this.coords = tilePos;
+	}
+	
+	public void translate (int dx, int dy) {
+		this.coords.translate(dx, dy);
+	}
+	
+	public Point getCoords() {
+		return this.coords;
+	}
+	
+	public int getNumber() {
+		return this.number;
+	}
+	
+	
+}
