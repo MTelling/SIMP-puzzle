8b258fb22feffcd7c6322dfe62c0b37c36a5ee34
Morten Telling <mtelling1995@gmail.com>
2016-01-13 00:07:02 +0100

    made randomiser a class of its own.

--- src/Board.java
+++ src/Board.java
@@ -68,121 +68,7 @@ public class Board implements Serializable {
 	}
 	
 	/// Methods for randomizer ///
-	private void moveEmptyTo(Point target, Point emptyTile, LinkedList<Move> moveSequence) {
-		
-		while(emptyTile.x < target.x) {
-			makePseudoMove(moveSequence, new Move(1, 0), emptyTile);
-		}
-		
-		while(emptyTile.x > target.x) {
-			makePseudoMove(moveSequence, new Move(-1, 0), emptyTile);
-		}
-		
-		while(emptyTile.y < target.y) {
-			makePseudoMove(moveSequence, new Move(0, 1), emptyTile);
-		}
-		
-		while(emptyTile.y > target.y) {
-			makePseudoMove(moveSequence, new Move(0, -1), emptyTile);
-		}
-		
-	}
-
-	private void makePseudoMove(LinkedList<Move> moveSequence, Move move, Point emptyTile) {
-		emptyTile.translate(move.dx, move.dy);
-		moveSequence.add(move);
-	}
-
-	private void generateRandomMovesInCloseArea(LinkedList<Move> moveSequence, Point emptyTile, int numberOfMoves) {
-	//Move x, then y, then x etc. Was x moved last?
-	 boolean justMovedX = false;
-	 Random rand = new Random();
-	 for(int i = 0; i < numberOfMoves; i++) {
-		 
-		//Valid dx and dy.
-		int[] validNums = { -1 , 1 };
-		
-		boolean hasNotMoved = true;
-		while(hasNotMoved) {
-			
-			int randomNumber = validNums[rand.nextInt(2)];
-			
-			Move xMove = new Move(randomNumber, 0);
-			Move yMove = new Move(0, randomNumber);
-			
-			if (isMoveValid(xMove, emptyTile) && !justMovedX) {
-				makePseudoMove(moveSequence, xMove, emptyTile);
-				justMovedX = true;
-				hasNotMoved = false;
-			} else if (isMoveValid(yMove, emptyTile) && justMovedX ) {
-				
-				makePseudoMove(moveSequence, yMove, emptyTile);
-				
-				justMovedX = false;
-				hasNotMoved = false;
-			}
-
-		}
-	 }
-	}
-
-	//Returns a list of movements required to randomize the current board. 
-	public LinkedList<Move> getRandomizingMoveSequence() {
-		LinkedList<Move> moveSequence = new LinkedList<Move>();
-		Point empty = ObjectCopy.point(currEmptyTile);
-		
-
-		int difficulty = Window.getSettings().getDifficulty();
-		//If the board is larger than 8, the difficulty shouldn't be linear. Only if difficulty is easy. 
-		if  (tilesPerRow > 8 && difficulty != Difficulty.EASY.getValue()) {
-			difficulty += 1 +  (this.tiles.length % 10);
-		}
-
-		//Create queue of places to visit
-		Stack<Point> placesToVisit = new Stack<>();
-		
-		int size = tilesPerRow - 1;
-		int quarterSize = size/4;
-		
-		//Calculate x and y points for all corners, center of quadrants and center of board.
-		Point center = new Point(size/2, size/2);
-		Point upperLeftCorner = new Point(0, 0);
-		Point upperRightCorner = new Point(size, 0); 
-		Point lowerLeftCorner = new Point(0, size);
-		Point lowerRightCorner = new Point(size,size);
-		Point secondQuadrant = new Point(quarterSize, quarterSize);
-		Point thirdQuadrant = new Point(quarterSize, size - quarterSize);
-		Point fourthQuadrant = new Point(size - quarterSize, size - quarterSize);
-		Point firstQuadrant = new Point(size - quarterSize, quarterSize);
-		
-		//Make a list of all those points.
-		Point[] points = {center, upperLeftCorner, upperRightCorner, lowerLeftCorner, lowerRightCorner, secondQuadrant, thirdQuadrant, fourthQuadrant, firstQuadrant};
-		
-		//For difficulty add the points a number of times. 
-		for (int i = 0; i < difficulty; i++) {
-			//Shuffle the list and then add all the points to the visitStack.
-			Collections.shuffle(Arrays.asList(points));
-
-			for (Point point: points) {
-				placesToVisit.push(point);
-			}		
-		}
-		
-		//Visit each place in the visitStack and on each place make a lot of random moves in the area around the point.
-		Point next;
-		while(!placesToVisit.isEmpty()) {
-			next = placesToVisit.pop();
-			moveEmptyTo(next, empty, moveSequence);
-			int areaSizeToRandomize = difficulty*difficulty;
-			//If the board is bigger than 25, it needs to make more moves unless difficulty is easy. 
-			if  (tilesPerRow > 25 && difficulty != Difficulty.EASY.getValue()) {
-				areaSizeToRandomize *= difficulty;
-			}
-			generateRandomMovesInCloseArea(moveSequence, empty, areaSizeToRandomize);
-		}
-		
-		return moveSequence;
-	}
+	
 
 	//Moves tile to the currEmptyTile position and swaps in tileArray + sets what the nextEmptyTile should be.
 	//This activates the animationStae
@@ -276,7 +162,7 @@ public class Board implements Serializable {
 	}
 	
 	//Method to determine if a move can be made from a given empty tile in a given board size. 
-	private boolean isMoveValid(Move move, Point emptyTile) {
+	public boolean isMoveValid(Move move, Point emptyTile) {
 		boolean shouldMove = false;
 		if (move.dx == -1) { //Right arrow
 			shouldMove = (emptyTile.x > 0) ? true : false;
@@ -290,7 +176,10 @@ public class Board implements Serializable {
 		return shouldMove;
 	}
 
-	
+	public LinkedList<Move> getRandomizingMoveSequence() {
+		Randomizer randomBoardGenerator = new Randomizer(this);
+		return randomBoardGenerator.getRandomizingMoveSequence();
+	}
 	
 	/// Getters from here ///
 	
---
+++ src/Randomizer.java
@@ -0,0 +1,134 @@
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Stack;
+
+public class Randomizer {
+	private Point currEmptyTile;
+	private int tilesPerRow;
+	private Board board;
+
+	public Randomizer(Board board) {
+		this.board = board;
+		this.currEmptyTile = ObjectCopy.point(board.getCurrEmptyTile());
+		this.tilesPerRow = board.getTilesPerRow();
+
+	}
+
+	private void moveEmptyTo(Point target, Point emptyTile, LinkedList<Move> moveSequence) {
+
+		while(emptyTile.x < target.x) {
+			makePseudoMove(moveSequence, new Move(1, 0), emptyTile);
+		}
+
+		while(emptyTile.x > target.x) {
+			makePseudoMove(moveSequence, new Move(-1, 0), emptyTile);
+		}
+
+		while(emptyTile.y < target.y) {
+			makePseudoMove(moveSequence, new Move(0, 1), emptyTile);
+		}
+
+		while(emptyTile.y > target.y) {
+			makePseudoMove(moveSequence, new Move(0, -1), emptyTile);
+		}
+
+	}
+
+	private void makePseudoMove(LinkedList<Move> moveSequence, Move move, Point emptyTile) {
+		emptyTile.translate(move.dx, move.dy);
+		moveSequence.add(move);
+	}
+
+	private void generateRandomMovesInCloseArea(LinkedList<Move> moveSequence, Point emptyTile, int numberOfMoves) {
+		//Move x, then y, then x etc. Was x moved last?
+		boolean justMovedX = false;
+		Random rand = new Random();
+		for(int i = 0; i < numberOfMoves; i++) {
+
+			//Valid dx and dy.
+			int[] validNums = { -1 , 1 };
+
+			boolean hasNotMoved = true;
+			while(hasNotMoved) {
+
+				int randomNumber = validNums[rand.nextInt(2)];
+
+				Move xMove = new Move(randomNumber, 0);
+				Move yMove = new Move(0, randomNumber);
+
+				if (board.isMoveValid(xMove, emptyTile) && !justMovedX) {
+					makePseudoMove(moveSequence, xMove, emptyTile);
+					justMovedX = true;
+					hasNotMoved = false;
+				} else if (board.isMoveValid(yMove, emptyTile) && justMovedX ) {
+
+					makePseudoMove(moveSequence, yMove, emptyTile);
+
+					justMovedX = false;
+					hasNotMoved = false;
+				}
+
+			}
+		}
+	}
+
+	//Returns a list of movements required to randomize the current board. 
+	public LinkedList<Move> getRandomizingMoveSequence() {
+		LinkedList<Move> moveSequence = new LinkedList<Move>();
+		Point empty = currEmptyTile;
+
+		int difficulty = Window.getSettings().getDifficulty();
+		//If the board is larger than 8, the difficulty shouldn't be linear. Only if difficulty is easy. 
+		if  (tilesPerRow > 8 && difficulty != Difficulty.EASY.getValue()) {
+			difficulty += 1 +  (tilesPerRow % 10);
+		}
+
+		//Create queue of places to visit
+		Stack<Point> placesToVisit = new Stack<>();
+
+		int size = tilesPerRow - 1;
+		int quarterSize = size/4;
+
+		//Calculate x and y points for all corners, center of quadrants and center of board.
+		Point center = new Point(size/2, size/2);
+		Point upperLeftCorner = new Point(0, 0);
+		Point upperRightCorner = new Point(size, 0); 
+		Point lowerLeftCorner = new Point(0, size);
+		Point lowerRightCorner = new Point(size,size);
+		Point secondQuadrant = new Point(quarterSize, quarterSize);
+		Point thirdQuadrant = new Point(quarterSize, size - quarterSize);
+		Point fourthQuadrant = new Point(size - quarterSize, size - quarterSize);
+		Point firstQuadrant = new Point(size - quarterSize, quarterSize);
+
+		//Make a list of all those points.
+		Point[] points = {center, upperLeftCorner, upperRightCorner, lowerLeftCorner, lowerRightCorner, secondQuadrant, thirdQuadrant, fourthQuadrant, firstQuadrant};
+
+		//For difficulty add the points a number of times. 
+		for (int i = 0; i < difficulty; i++) {
+			//Shuffle the list and then add all the points to the visitStack.
+			Collections.shuffle(Arrays.asList(points));
+
+			for (Point point: points) {
+				placesToVisit.push(point);
+			}		
+		}
+
+		//Visit each place in the visitStack and on each place make a lot of random moves in the area around the point.
+		Point next;
+		while(!placesToVisit.isEmpty()) {
+			next = placesToVisit.pop();
+			moveEmptyTo(next, empty, moveSequence);
+			int areaSizeToRandomize = difficulty*difficulty;
+			//If the board is bigger than 25, it needs to make more moves unless difficulty is easy. 
+			if  (tilesPerRow > 25 && difficulty != Difficulty.EASY.getValue()) {
+				areaSizeToRandomize *= difficulty;
+			}
+			generateRandomMovesInCloseArea(moveSequence, empty, areaSizeToRandomize);
+		}
+
+		return moveSequence;
+	}
+}
