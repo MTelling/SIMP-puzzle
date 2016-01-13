4e316830aa1db147bc11a54e58b53495e6238353
Tobias Lindstrøm <Toli.TobiasLindstrom@gmail.com>
2016-01-13 18:09:51 +0100

    Fixed highscore loading issue

--- src/Highscore.java
+++ src/Highscore.java
@@ -16,7 +16,7 @@ public class Highscore implements Serializable {
 	
 	public Highscore(String diff) {
 		this.difficulty = diff;
-		if(new File(FILE_NAME + this.difficulty + SaveLoad.FILE_EXT).exists()) {
+		if(new File(FILE_NAME + this.difficulty + "." + SaveLoad.FILE_EXT).exists()) {
 			this.loadHighscores();
 		} else {
 			for(int i = 3; i <= 100; i++) {
